86930b9caacc271eef863f72caa700095fbfd309
Your Name <you@example.com>
2025-09-06 01:14:08 -0400

    feat(networking): add subnet

--- variables.tf
+++ variables.tf
@@ -1,3 +1,16 @@
 variable "vpc_cidr" {
   type = string
 }
+
+variable "subnet_cidr" {
+  type = string
+}
+
+resource "aws_subnet" "tf_cloud" {
+  cidr_block = var.subnet_cidr
+  vpc_id = aws_vpc.tf_cloud.id
+
+  tags = {
+    Name = "terraform-cloud"
+  }
+}
